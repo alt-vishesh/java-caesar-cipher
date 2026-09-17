0641c306e7f9d618b4eb3b24ebe372972a94ff99
alt-vishesh <vishesh281007@gmail.com>
2026-09-18 00:19:56 +0530

    Added Caesar Cipher project

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a message: ");
+        String message = sc.nextLine();
+        System.out.print("Enter a shift value: ");
+        int shift = sc.nextInt();
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (c >= 'A' && c <= 'Z')
+                encrypted.append((char)((c - 'A' + shift) % 26 + 'A'));
+            else if (c >= 'a' && c <= 'z')
+                encrypted.append((char)((c - 'a' + shift) % 26 + 'a'));
+            else
+                encrypted.append(c);
+        }
+        System.out.println("Encrypted message: " + encrypted);
+        sc.close();
+    }
+}
